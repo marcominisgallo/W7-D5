4f8e5035460a02020941f8258fe95496bcfeacc7
Marco <marcominisgallo@yahoo.it>
2025-03-21 12:00:18 +0100

    Home & BOffice

---
+++ Scripts/BOffice.js
@@ -0,0 +1,95 @@
+const ApiKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2RkMmE2NDM4MzRiZjAwMTUwMDA3MDIiLCJpYXQiOjE3NDI1NDc1NTYsImV4cCI6MTc0Mzc1NzE1Nn0.Qq5YAAgqKvzBcpoY_W16XAtBDEE4OG-wpiCguKXLvXc";
+
+class wineC {
+  constructor(_name, _brand, _description, _price, _imgUrl) {
+    this.name = _name;
+    this.brand = _brand;
+    this.description = _description;
+    this.price = _price;
+    this.imgUrl = _imgUrl;
+  }
+}
+
+const URLparameters = new URLSearchParams(location.search);
+const wineId = URLparameters.get("id");
+
+const nameInput = document.getElementById("name");
+const brandInput = document.getElementById("brand");
+const descriptionInput = document.getElementById("description");
+const priceInput = document.getElementById("price");
+const imgUrlInput = document.getElementById("imgUrl");
+
+const winesURL = "https://striveschool-api.herokuapp.com/api/product/";
+
+if (wineId) {
+  fetch(winesURL + "/" + wineId)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("errore nella fetch");
+      }
+    })
+    .then((data) => {
+      nameInput.value = data.name;
+      brandInput.value = data.brand;
+      descriptionInput.value = data.description;
+      priceInput.value = data.price;
+      imgUrlInput.value = data.imgUrl;
+    })
+    .catch((err) => console.log("ERRORE", err));
+}
+
+const form = document.getElementById("wine-form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const wine = new wineC(
+    nameInput.value,
+    brandInput.value,
+    descriptionInput.value,
+    priceInput.value,
+    imgUrlInput.value
+  );
+
+  console.log("WINE", wine);
+
+  let metodo;
+  let URLdaUsare;
+
+  if (wineId) {
+    metodo = "PUT";
+    URLdaUsare = winesURL + "/" + wineId;
+  } else {
+    metodo = "POST";
+    URLdaUsare = winesURL;
+  }
+
+  fetch(URLdaUsare, {
+    method: metodo,
+    body: JSON.stringify(wine),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: ApiKey,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("SALVATAGGIO VINO COMPLETATO!");
+        form.reset();
+      } else {
+        throw new Error("response non OK");
+      }
+    })
+    .catch((err) => {
+      console.log("errore nel salvataggio!", err);
+    });
+});
+
+const printDateInFooter = function () {
+  const footerSpan = document.getElementById("year");
+  footerSpan.innerText = new Date().getFullYear();
+};
+
+printDateInFooter();
---
+++ Scripts/HomePage.js
@@ -0,0 +1,58 @@
+const nascondiCaricam = function () {
+  const div = document.getElementById("spinnerDiv");
+  div.classList.add("d-none");
+};
+
+const ApiKey =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2RkMmE2NDM4MzRiZjAwMTUwMDA3MDIiLCJpYXQiOjE3NDI1NDc1NTYsImV4cCI6MTc0Mzc1NzE1Nn0.Qq5YAAgqKvzBcpoY_W16XAtBDEE4OG-wpiCguKXLvXc";
+
+const getWines = function () {
+  const winesURL = "https://striveschool-api.herokuapp.com/api/product/";
+
+  fetch(winesURL, {
+    headers: {
+      Authorization: ApiKey,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json;
+      } else {
+        throw new Error("La risposta non è andata a buon fine");
+      }
+    })
+    .then((data) => {
+      nascondiCaricam();
+      console.log("DATA", data);
+
+      const row = document.getElementById("allWine-row");
+      data.forEach((wine) => {
+        row.innerHTML = `
+          <div class="col col-12 col-lg-3 col-md-4 col-sm-6">
+            <div class="card">
+              <img src=${wine.imgUrl} alt="Wine-Pic" />
+              <div class="card-body">
+                <h5 class="card-title">${wine.name}</h5>
+                <p class="card-text">${wine.description}</p>
+                <p class="card-text">${wine.price}€</p>
+                <a href="./details.html?id=${wine._id}" class="btn btn-primary">Vai ai dettagli</a>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+    })
+    .catch((error) => {
+      nascondiCaricam();
+      console.log("ERRORE", error);
+    });
+};
+
+getWines();
+
+const printDateInFooter = function () {
+  const footerSpan = document.getElementById("year");
+  footerSpan.innerText = new Date().getFullYear();
+};
+
+printDateInFooter();
